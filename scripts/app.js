7f1ace818ad4dbd7f394cd21e52e049f7ba878ad
= <danialhbi02@gmail.com>
2023-07-01 11:55:38 +0330

    js site Last Update

--- scripts/app.js
+++ scripts/app.js
@@ -14,17 +14,70 @@ menuBtn.addEventListener("click", function () {
 
 // New...
 
-// header_Step
-var header_img = [
+function renderImages(data,id) {
+  const container = document.getElementById(id);
+
+  data.map((item) => {
+    const div = document.createElement("nav-logo");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+
+    img.src = item.src;
+    p.textContent = item.text;
+    // div.classList.add("header-content__right"); // اضافه کردن کلاس به دیو
+    div.appendChild(img);
+    div.appendChild(p);
+    container.appendChild(div);
+  });
+}
+
+const header_section = [
   {
-    name: "header_img",
-    src: "../images/header-img.svg",
-  },
-  {
-    name: "Interactive_img",
-    src: "../images/Interactive-ART.png",
+    src: "images/header-img.svg",
   },
 ];
+const nav_section = [
+
+  {
+    src: "images/Interactive-ART.png",
+  },
+];
+
+const main_section = [
+{
+  src:"images/main-img.svg",
+}
+];
+const newsletter_section = [
+{
+  src:"images/right-arrow.svg",
+}
+];
+const comment_person_1 = [
+  {
+    src:"images/person-1.png",
+  }
+  ];
+  const comment_person_2 = [
+    {
+      src:"images/person-2.png",
+    }
+    ];
+    const comment_person_3 = [
+      {
+        src:"images/person-3.png",
+      }
+      ];
+;
+
+// header_Step
+renderImages(header_section,"header-js");
+renderImages(nav_section,"nav-logo")
+renderImages(main_section,"main-js");
+renderImages(newsletter_section,"newsletter-img")
+renderImages(comment_person_1,"comment_images")
+renderImages(comment_person_2,"comment_images2")
+renderImages(comment_person_3,"comment_images3")
 var header_text = [
   {
     name: "nav text",
@@ -46,12 +99,7 @@ var header_text = [
 
 // Main_Step
 
-var main_img = [
-  {
-    name: "main img",
-    src: "../images/main-img.svg",
-  },
-];
+
 
 var main_text = [
   {
@@ -66,22 +114,170 @@ var main_text = [
 
 // Comment_ Step
 
-var comment_img = [
+
+var comment_img_2 = [
   {
-    name: "comment img",
-    src1: "../images/person-1.png",
-    src2: "../images/person-2.png",
-    src3: "../images/person-3.png",
-  },
-  {
-    name: "logo",
-    src1: "../images/logo-1.svg",
-    src2: "../images/logo-2.svg",
-    src3: "../images/logo-3.svg",
-    src4: "../images/logo-4.svg",
-    src5: "../images/logo-5.svg",
+    src1: "images/logo-1.svg",
+    src2: "images/logo-2.svg",
+    src3: "images/logo-3.svg",
+    src4: "images/logo-4.svg",
+    src5: "images/logo-5.svg",
   },
 ];
+
+const comment_container = [
+  {
+    commentText: "",
+    commentImg: "images/logo-1.svg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/logo-2.svg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/logo-3.svg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/logo-4.svg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/logo-5.svg",
+    commentAuthor: "",
+  },
+];
+// portfolio
+const portfolio_container = [
+  {
+    commentText: "",
+    commentImg: "images/portfolio-1.jpg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/portfolio-2.jpg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/portfolio-3.jpg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/portfolio-4.jpg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/portfolio-5.jpg",
+    commentAuthor: "",
+  },
+  {
+    commentText: "",
+    commentImg: "images/portfolio-6.jpg",
+    commentAuthor: "",
+  },
+];
+// const comment_person_container = [
+//   {
+//     commentText: "",
+//     commentImg: "images/person-1.png",
+//     commentAuthor: "",
+//   },
+//   {
+//     commentText: "",
+//     commentImg: "images/person-1.png",
+//     commentAuthor: "",
+//   },
+//   {
+//     commentText: "",
+//     commentImg: "images/person-1.png",
+//     commentAuthor: "",
+//   },
+// ];
+commentRendering(comment_container,"js-photo");
+function commentRendering(comment_container,id) {
+  var test1 = document.getElementById(id);
+  console.log(test1);
+  comment_container.map((r) => {
+    console.log(r)
+    const comment_Text = document.createElement("p");
+    comment_Text.innerText = r.commentText;
+    const commentAuthor = document.createElement("p");
+    commentAuthor.innerHTML = r.commentAuthor;
+    const commentImg = document.createElement("img");
+    commentImg.src = r.commentImg;
+    const commentbox_top = document.createElement("div");
+    commentbox_top.appendChild(comment_Text);
+    const commentbox_bottom = document.createElement("div");
+    commentbox_bottom.appendChild(commentAuthor);
+    commentbox_bottom.appendChild(commentImg);
+    const commentBox = document.createElement("div");
+    commentBox.classList.add("comment-box");
+
+    commentBox.appendChild(commentbox_top);
+    commentBox.appendChild(commentbox_bottom);
+    test1.appendChild(commentBox)
+  });
+}
+
+portfolioRendering(portfolio_container,"portfolio-js-picture");
+function portfolioRendering(portfolio_container,id) {
+  var test1 = document.getElementById(id);
+  console.log(test1);
+  portfolio_container.map((r) => {
+    console.log(r)
+    const comment_Text = document.createElement("p");
+    comment_Text.innerText = r.commentText;
+    const commentAuthor = document.createElement("p");
+    commentAuthor.innerHTML = r.commentAuthor;
+    const commentImg = document.createElement("img");
+    commentImg.src = r.commentImg;
+    const commentbox_top = document.createElement("div");
+    commentbox_top.appendChild(comment_Text);
+    const commentbox_bottom = document.createElement("div");
+    commentbox_bottom.appendChild(commentAuthor);
+    commentbox_bottom.appendChild(commentImg);
+    const commentBox = document.createElement("div");
+    commentBox.classList.add("comment-box");
+
+    commentBox.appendChild(commentbox_top);
+    commentBox.appendChild(commentbox_bottom);
+    test1.appendChild(commentBox)
+  });
+}
+// function CommentPersonRendering(comment_person_container,id) {
+//   var test1 = document.getElementById(id);
+//   console.log(test1);
+//   comment_person_container.map((r) => {
+//     console.log(r)
+//     const comment_Text = document.createElement("p");
+//     comment_Text.innerText = r.commentText;
+//     const commentAuthor = document.createElement("p");
+//     commentAuthor.innerHTML = r.commentAuthor;
+//     const commentImg = document.createElement("img");
+//     commentImg.src = r.commentImg;
+//     const commentbox_top = document.createElement("div");
+//     commentbox_top.appendChild(comment_Text);
+//     const commentbox_bottom = document.createElement("div");
+//     commentbox_bottom.appendChild(commentAuthor);
+//     commentbox_bottom.appendChild(commentImg);
+//     const commentBox = document.createElement("div");
+//     commentBox.classList.add("comment-box");
+
+//     commentBox.appendChild(commentbox_top);
+//     commentBox.appendChild(commentbox_bottom);
+//     test1.appendChild(commentBox)
+//   });
+// }
+// CommentPersonRendering(comment_person_container,"comment_images");
 
 var comment_text = [
   {
@@ -95,111 +291,28 @@ var comment_text = [
   },
 ];
 
-
-
-
 // Portfolio_step
 
 
 
-var portfolio_img = [
-
-  {
-    name:"portfolio img",
-    src1:"../images/portfolio-1.jpg",
-    src2:"../images/portfolio-2.jpg",
-    src3:"../images/portfolio-3.jpg",
-    src4:"../images/portfolio-4.jpg",
-    src5:"../images/portfolio-5.jpg",
-    src6:"../images/portfolio-6.jpg",
-
-  },
-
-];
-
 var portfolio_text = [
-
   {
-    name:"portfolio text",
-    text1:"PORTFOLIO",
-    text2:"Three Simple Step To Started Working Process",
-  }
+    name: "portfolio text",
+    text1: "PORTFOLIO",
+    text2: "Three Simple Step To Started Working Process",
+  },
 ];
 
-var footer_img = [
 
-  {
-    name:"footer img",
-    src:"../images/right-arrow.svg",
-  }
-];
 
 var footer_text = [
-
   {
-    name:"footer text",
-    text1:"Create a new generation website for your business.",
-    text2:"Copyright © 2018-2019",
-    text3:"Interactive ART Company All rights reserved",
-  }
+    name: "footer text",
+    text1: "Create a new generation website for your business.",
+    text2: "Copyright © 2018-2019",
+    text3: "Interactive ART Company All rights reserved",
+  },
 ];
-var element1 = document.getElementById("right_image");
-element1.src = header_img[0]["src"];
-
-// 2
-var element2 = document.getElementById("nav_logos");
-element2.src = header_img[1]["src"];
-
-// 3
-var element3 = document.getElementById("main_img");
-element3.src = main_img[0]["src"];
-
-// person_img
-
-var element3 = document.getElementById("person_1");
-element3.src = comment_img[0]["src1"];
-var element4 = document.getElementById("person_2");
-element4.src = comment_img[0]["src2"];
-var element5 = document.getElementById("person_3");
-element5.src = comment_img[0]["src3"];
-
-// logo_img
-
-var element6 = document.getElementById("logo_1");
-element6.src = comment_img[1]["src1"];
-var element7 = document.getElementById("logo_2");
-element7.src = comment_img[1]["src2"];
-var element8 = document.getElementById("logo_3");
-element8.src = comment_img[1]["src3"];
-var element9 = document.getElementById("logo_4");
-element9.src = comment_img[1]["src4"];
-var element10 = document.getElementById("logo_5");
-element10.src = comment_img[1]["src5"];
-
-// portfolio_img
-
-
-var element11 = document.getElementById("portfolio_1");
-element11.src = portfolio_img[0]['src1'];
-var element12 = document.getElementById("portfolio_2");
-element12.src = portfolio_img[0]['src2'];
-var element13 = document.getElementById("portfolio_3");
-element13.src = portfolio_img[0]['src3'];
-var element14 = document.getElementById("portfolio_4");
-element14.src = portfolio_img[0]['src4'];
-var element15 = document.getElementById("portfolio_5");
-element15.src = portfolio_img[0]['src5'];
-var element16 = document.getElementById("portfolio_6");
-element16.src = portfolio_img[0]['src6'];
-
-// footer
-
-var element17 = document.getElementById("right_arrow");
-element17.src = footer_img[0]['src'];
-
-// All Text
-
-//
 
 var textelement = document.getElementById("menu__link_js");
 textelement.innerText = header_text[0]["text1"];
@@ -250,18 +363,28 @@ textelement.innerText = comment_text[0]["text3"];
 var textelement = document.getElementById("comment__body3");
 textelement.innerText = comment_text[0]["text3"];
 var textelement = document.getElementById("comment_person");
-textelement.innerText = comment_text[0]['text4'];
+textelement.innerText = comment_text[0]["text4"];
 var textelement = document.getElementById("comment_person2");
-textelement.innerText = comment_text[0]['text4'];
+textelement.innerText = comment_text[0]["text4"];
 var textelement = document.getElementById("comment_person3");
-textelement.innerText = comment_text[0]['text4'];
+textelement.innerText = comment_text[0]["text4"];
 var textelement = document.getElementById("portfolio__text");
-textelement.innerText = portfolio_text[0]['text1'];
+textelement.innerText = portfolio_text[0]["text1"];
 var textelement = document.getElementById("portfolio__title");
-textelement.innerText = portfolio_text[0]['text2'];
+textelement.innerText = portfolio_text[0]["text2"];
 var textelement = document.getElementById("newsletter__text");
-textelement.innerText = footer_text[0]['text1'];
+textelement.innerText = footer_text[0]["text1"];
 var textelement = document.getElementById("copy-right__text");
-textelement.innerText = footer_text[0]['text2'];
+textelement.innerText = footer_text[0]["text2"];
 var textelement = document.getElementById("copy-right__text2");
-textelement.innerText = footer_text[0]['text3'];
+textelement.innerText = footer_text[0]["text3"];
+
+
+const images = [
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 1 },
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 2 },
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 3 },
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 4 },
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 5 },
+  { src: "images/portfolio-1.jpg", alt: "car-image", key: 6 },
+];
